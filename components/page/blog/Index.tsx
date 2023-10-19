08383cda4832a5345a2ebe840edfa0b85ff5f730
pparksang <pparksang1013@gmail.com>
2023-10-19 23:58:42 +0900

    💅Style Index css
    - Index css 추가

--- components/page/blog/Index.tsx
+++ components/page/blog/Index.tsx
@@ -2,13 +2,16 @@ import { allPosts, Post } from "@/.contentlayer/generated";
 import GetPostContent from "./GetPostContent";
 import { compareDesc } from "date-fns";
 
+// 💅CSS
+import style from "./blog.module.css";
+
 function Blog() {
     const sortDate = allPosts.sort((a, b) => {
         return compareDesc(new Date(a.date), new Date(b.date));
     });
 
     return (
-        <div>
+        <div className={style.wrapper}>
             {sortDate.map((ele: Post, i: number) => {
                 return (
                     <GetPostContent

c56dcd0884b2430107f50f19d9ee3222a3861bfa
pparksang <pparksang1013@gmail.com>
2023-10-19 17:22:16 +0900

    ⭐️Feat Add logo

---
+++ components/layout/header/Index.tsx
@@ -0,0 +1,16 @@
+import Link from "next/link";
+
+function Header() {
+    return (
+        <header>
+            <Link href="/">
+                <div>
+                    <span>i</span>
+                    <span>mp</span>ort
+                </div>
+            </Link>
+        </header>
+    );
+}
+
+export default Header;

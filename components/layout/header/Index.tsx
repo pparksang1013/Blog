2183681624469948e85c759e2507e77adebd11f0
pparksang <pparksang1013@gmail.com>
2023-10-19 17:24:35 +0900

    💅Style Header style

--- components/layout/header/Index.tsx
+++ components/layout/header/Index.tsx
@@ -1,12 +1,13 @@
 import Link from "next/link";
+import style from "./header.module.css";
 
 function Header() {
     return (
-        <header>
-            <Link href="/">
+        <header className={style.wrapper}>
+            <Link href="/" className={style.logo}>
                 <div>
-                    <span>i</span>
-                    <span>mp</span>ort
+                    <span className={style.fontSizeDown}>i</span>
+                    <span className={style.mp_letterStyle}>mp</span>ort
                 </div>
             </Link>
         </header>

e9b36c7288a8e8d1bac15f9513969f676c2aa1be
pparksang <pparksang1013@gmail.com>
2023-10-19 16:25:44 +0900

    📦Setting Contentlayer 설정
    Contentlayer 설정추가

--- next.config.js
+++ next.config.js
@@ -1,4 +1,6 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
+const { withContentlayer } = require("next-contentlayer");
 
-module.exports = nextConfig
+/** @type {import('next').NextConfig} */
+const nextConfig = { reactStrictMode: true, swcMinify: true, pageExtensions: ["ts", "tsx", "js", "jsx", "md", "mdx"] };
+
+module.exports = withContentlayer(nextConfig);

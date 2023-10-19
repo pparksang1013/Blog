2ff382afdc65e821e73d5b85786e77f322057b85
pparksang <pparksang1013@gmail.com>
2023-10-20 00:22:55 +0900

    ⭐️Feat Post tag
    - 각 프로그래밍 언어에 맞는 tag 기능 추가
    - 아직 모든 언어를 추가하지는 않았다.

--- components/page/blog/PostTag.tsx
+++ components/page/blog/PostTag.tsx
@@ -1,1 +1,24 @@
+interface tagType {
+    tag: string;
+}
 
+function PostTag(props: tagType) {
+    const { tag } = props;
+
+    let tagColorBox: string | null = null;
+    let tagCircle: string | null = null;
+
+    const sortTag = () => {
+        if (tag === "react") {
+            return (
+                <div>
+                    <p>react</p>
+                </div>
+            );
+        }
+    };
+
+    return sortTag();
+}
+
+export default PostTag;

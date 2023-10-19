dfb7202dfac90e575c8fa937ea30afe99359cc53
pparksang <pparksang1013@gmail.com>
2023-10-19 23:52:43 +0900

    ⭐️Feat Contentlayer post
    - Contentlayer post 파일을 불러온다.
    - Date-fns 라이브러리를 통해 posts를 날짜별로 정렬한다.
    - 정렬한 posts를 GetPostContent component로 정보를 보낸다.

--- components/page/blog/Index.tsx
+++ components/page/blog/Index.tsx
@@ -1,1 +1,28 @@
+import { allPosts, Post } from "@/.contentlayer/generated";
+import GetPostContent from "./GetPostContent";
+import { compareDesc } from "date-fns";
 
+function Blog() {
+    const sortDate = allPosts.sort((a, b) => {
+        return compareDesc(new Date(a.date), new Date(b.date));
+    });
+
+    return (
+        <div>
+            {sortDate.map((ele: Post, i: number) => {
+                return (
+                    <GetPostContent
+                        tag={ele.tag}
+                        title={ele.title}
+                        path={ele._raw.sourceFileName}
+                        date={ele.date}
+                        summary={ele.summary}
+                        key={i}
+                    />
+                );
+            })}
+        </div>
+    );
+}
+
+export default Blog;

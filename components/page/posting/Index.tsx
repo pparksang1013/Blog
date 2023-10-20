e49814786edc5dfa91beac591bee5899425102c3
pparksang <pparksang1013@gmail.com>
2023-10-20 12:01:29 +0900

    ⭐️Feat Contentlayer useMDXComponent
    - Contentlayer을 사용해 MDX render

--- components/page/posting/Index.tsx
+++ components/page/posting/Index.tsx
@@ -1,4 +1,5 @@
 import { allPosts, Post } from "@/.contentlayer/generated";
+import { useMDXComponent } from "next-contentlayer/hooks";
 
 export function generateStaticParams() {
     const staticPath = allPosts.map((post: Post) => {
@@ -14,9 +15,12 @@ function BlogPosting({ params }: { params: { slug: string } }) {
         return replacePath === params.slug;
     });
 
+    const MDXComponet = useMDXComponent(findPath.body.code);
+
     return (
         <div>
             <h1>{findPath.title}</h1>
+            <MDXComponet />
         </div>
     );
 }

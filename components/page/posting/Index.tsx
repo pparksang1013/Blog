41cf3e5db2e64334f15bfaee732090ee1b5619ab
pparksang <pparksang1013@gmail.com>
2023-10-20 11:58:33 +0900

    ⭐️Feat StaticParams
    - 블로그 링크 정적 생성

--- components/page/posting/Index.tsx
+++ components/page/posting/Index.tsx
@@ -1,5 +1,13 @@
 import { allPosts, Post } from "@/.contentlayer/generated";
 
+export function generateStaticParams() {
+    const staticPath = allPosts.map((post: Post) => {
+        return post._raw.flattenedPath;
+    });
+
+    return staticPath;
+}
+
 function BlogPosting({ params }: { params: { slug: string } }) {
     const findPath: any = allPosts.find((post: Post) => {
         const replacePath = post._raw.sourceFileName.replace(".mdx", "");

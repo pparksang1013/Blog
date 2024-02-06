7b42943615ec2a03259bc677821670eb9f8ccb2c
pparksang <pparksang1013@gmail.com>
2024-02-06 15:44:00 +0900

    deleted

--- lib/isKoreanOrEnglish.ts
+++
@@ -1,9 +0,0 @@
-export function isKoreanOrEnglish(title: string) {
-    let firstWord = title.charAt(0);
-
-    if (/[ㄱ-ㅎㅏ-ㅣ가-힣]/.test(firstWord)) {
-        return "KR";
-    } else if (/[a-zA-Z]/.test(firstWord)) {
-        return "EN";
-    }
-}

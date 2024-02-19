e8c38f5870f21b3e358919bbe9aa91e0c726afdf
pparksang <pparksang1013@gmail.com>
2024-02-20 02:49:22 +0900

    🧩Refactoring

--- components/page/posting/CustomMDX.tsx
+++ components/page/posting/CustomMDX.tsx
@@ -1,41 +1,31 @@
 import type { MDXComponents } from "mdx/types";
 import React, { ReactElement } from "react";
 import Link from "next/link";
-import style from "../../../style/posting/mdx.module.css";
-import "../../../style/posting/maincateprops.css";
+import "../../../style/posting/mdx.css";
 
 const CustomMDX: MDXComponents = {
-    MainCate: ({ children, tag }: { children: React.ReactNode; tag: string }) => {
-        const contents = React.Children.toArray(children)
-            .filter((child): child is ReactElement => React.isValidElement(child) && child.type === "h2")
-            .map((ele, i) => {
-                return <Link href={`#${ele.props.id}`}>{ele.props.children}</Link>;
-            });
+    Wrapper: ({ children }: { children: React.ReactNode; tag: string }) => {
+        return <div className="posting_wrapper">{children}</div>;
+    },
 
+    FlexBox: ({ children, margin, maxWidth }: { children: React.ReactNode; margin?: string; maxWidth?: string }) => {
         return (
-            <section className={tag} id={style.mdx}>
-                <nav id={style.contents_box}>
-                    <div id={style.contents}>{contents}</div>
-                </nav>
+            <div id="flex_box" style={{ margin, maxWidth }}>
                 {children}
-            </section>
-        );
-    },
-
-    HighlightBox: ({ children }: { children: React.ReactNode }) => {
-        return <div className={`${style.highlight_box} highlight_box`}>{children}</div>;
-    },
-
-    SyntaxBox: ({ children }: { children: React.ReactNode }) => {
-        return <div id="syntax_box">{children}</div>;
-    },
-
-    CodeBox: () => {
-        return (
-            <div id={style.code}>
-                <strong>code</strong>
             </div>
         );
+    },
+
+    FlexItemWrapper: ({ children }: { children: React.ReactNode }) => {
+        return <div className="flex_item">{children}</div>;
+    },
+
+    CustomBox: ({ children, margin, maxWidth }: { children: React.ReactNode; margin?: string; maxWidth?: string }) => {
+        return <div style={{ margin, maxWidth }}>{children}</div>;
+    },
+
+    Highlight: ({ children }: { children: React.ReactNode }) => {
+        return <b id="highlight">{children}</b>;
     },
 };
 

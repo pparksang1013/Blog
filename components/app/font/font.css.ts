05864d762da1aa662d4958f09f73371533a98941
pparksang <pparksang1013@gmail.com>
2024-04-19 19:16:21 +0900

    🧩REFACTORING

--- components/app/font/font.css.ts
+++ components/app/font/font.css.ts
@@ -1,12 +1,10 @@
 import { globalFontFace, style } from "@vanilla-extract/css";
 
-const suit = "suit";
-
-globalFontFace(suit, {
+globalFontFace("suit", {
     src: `url('/SUIT-Variable.woff2') format('woff2')`,
     fontWeight: "variable",
 });
 
-export const font = style({
-    fontFamily: suit,
+export const suit = style({
+    fontFamily: "suit",
 });

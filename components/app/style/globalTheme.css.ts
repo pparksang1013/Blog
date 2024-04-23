ab68ad87469a97df3d25286110015a0da8726809
pparksang <pparksang1013@gmail.com>
2024-04-23 18:48:21 +0900

    🧩REFACTORING Theme 추가

--- components/app/style/globalTheme.css.ts
+++ components/app/style/globalTheme.css.ts
@@ -3,9 +3,18 @@ import { createGlobalTheme } from "@vanilla-extract/css";
 export const globalTheme = createGlobalTheme(":root", {
     fonts: {
         mainfont: `suit, serif`,
-        suit: `suit`,
+        gogh: "gogh",
     },
     colors: {
-        primary: "FCA61D",
+        primary: "#fca61d",
+        black: "#262626",
+        grey: "#a6a6a6",
+        blue: "#1f4eff",
+    },
+    rgb: {
+        primary: "252, 166, 29",
+        black: "38, 38, 38",
+        grey: "166, 166, 166",
+        blue: "31, 78, 255",
     },
 });

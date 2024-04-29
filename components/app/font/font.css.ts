8729672dc8c00a115cf25bba237258c2b5fa2277
pparksang <pparksang1013@gmail.com>
2024-04-29 19:55:41 +0900

    💊CHORE Hack 폰트 추가

--- components/app/font/font.css.ts
+++ components/app/font/font.css.ts
@@ -9,3 +9,8 @@ globalFontFace("gogh", {
     src: `url('/font/Gogh-ExtraBold.woff2') format('woff2')`,
     fontWeight: "900",
 });
+
+globalFontFace("hack", {
+    src: `url('/font/Hack-Regular.ttf') format('truetype')`,
+    fontWeight: 400,
+});

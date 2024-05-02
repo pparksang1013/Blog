ecc6b042de614149f1bbc84b61dee59ef2dc9230
pparksang <pparksang1013@gmail.com>
2024-05-02 12:18:35 +0900

    🧩REFACTORING Vanilla extract 자식요소 스타일 추가
    - global style 파일이 2개라, main page와 posting page 스타일이 깨지는 현상으로 인해 위 코드와 같이 변경

--- components/posting/style/customGlobalStyle.css.ts
+++ components/posting/style/customGlobalStyle.css.ts
@@ -1,7 +1,8 @@
 import { globalStyle } from "@vanilla-extract/css";
 import { globalTheme } from "@/components/app/style/globalTheme.css";
+import { styles } from "./postingContentsStyle.css";
 
-globalStyle("[data-rehype-pretty-code-title]", {
+globalStyle(`${styles.box} [data-rehype-pretty-code-title]`, {
     color: "#fff",
     backgroundColor: `rgba(${globalTheme.rgb.black}, 0.8)`,
     fontFamily: globalTheme.fonts.hack,
@@ -10,16 +11,16 @@ globalStyle("[data-rehype-pretty-code-title]", {
     marginTop: 10,
 });
 
-globalStyle("code span", {
+globalStyle(`${styles.box} code span`, {
     fontSize: "0.9em",
     fontFamily: globalTheme.fonts.hack,
 });
 
-globalStyle("ol", {
+globalStyle(`${styles.box} ol`, {
     counterReset: "counter",
 });
 
-globalStyle("h2", {
+globalStyle(`${styles.box} h2`, {
     color: globalTheme.colors.primary,
     marginBottom: 6,
     fontSize: 48,
@@ -28,27 +29,27 @@ globalStyle("h2", {
     letterSpacing: -1,
 });
 
-globalStyle("h3", {
+globalStyle(`${styles.box} h3`, {
     color: `rgba(${globalTheme.rgb.black}, 0.95)`,
     margin: "48px 0 48px",
     fontWeight: 800,
     fontSize: 38,
 });
 
-globalStyle("h4", {
+globalStyle(`${styles.box} h4`, {
     color: `rgba(${globalTheme.rgb.black}, 0.95)`,
     margin: "50px 0px 10px",
     fontSize: 30,
     letterSpacing: -1,
 });
 
-globalStyle("h5", {
+globalStyle(`${styles.box} h5`, {
     margin: "20px 0px",
     fontSize: 24,
     letterSpacing: -1,
 });
 
-globalStyle("h6", {
+globalStyle(`${styles.box} h6`, {
     margin: "10px 0px",
     fontSize: 19,
     letterSpacing: -1,

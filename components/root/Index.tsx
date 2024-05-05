2a2b49f020903b9e78bef154ac8be57a2882aa0f
pparksang <pparksang1013@gmail.com>
2024-05-05 23:21:48 +0900

    🔥FIX
    - non-null 단언 연산자로 이슈 해결

--- components/root/Index.tsx
+++ components/root/Index.tsx
@@ -2,7 +2,7 @@ import { notion } from "../../api/notion";
 
 export async function getStaticProps() {
     const getDatabaseQuery = await notion.databases.query({
-        database_id: process.env.NOTION_DB,
+        database_id: process.env.NOTION_DB!,
     });
 }
 

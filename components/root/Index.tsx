41828dea7cd1febccfa69dfa4dfea94e86f0a5b6
pparksang <pparksang1013@gmail.com>
2024-05-05 23:08:17 +0900

    👾ISSUE 타입이슈
    - .env 파일에 NOTION_DB 값이 string 값만 와야하는데, undefined 일 수 도 있어 발생한 타입이슈

---
+++ components/root/Index.tsx
@@ -0,0 +1,11 @@
+import { notion } from "../../api/notion";
+
+export async function getStaticProps() {
+    const getDatabaseQuery = await notion.databases.query({
+        database_id: process.env.NOTION_DB,
+    });
+}
+
+export const Root = () => {
+    return <div>index</div>;
+};

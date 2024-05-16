3e422942dc215af4c4cc66d3682d62c427ab3a46
pparksang <pparksang1013@gmail.com>
2024-05-17 01:07:15 +0900

    👾ISSUE :20 TODO 참고

--- components/posting/child/Paragraph.tsx
+++ components/posting/child/Paragraph.tsx
@@ -1,5 +1,28 @@
 import { RichTextType } from "@/type/notionRichtextType";
+import { postingContentsStyle } from "../style/postingContents.css";
+import { notionColorStyle } from "@/style/notionColor.css";
 
 export const Paragraph = ({ text }: { text: RichTextType }) => {
-    return text.rich_text.length !== 0 ? <p>{text.rich_text[0].plain_text}</p> : <br />;
+    if (!text.rich_text[0]) {
+        return <br />;
+    }
+
+    let textStyleArr = [postingContentsStyle.p.basic];
+
+    // 타입 단언
+    for (const key in text.rich_text[0].annotations) {
+        if (text.rich_text[0].annotations[key] === true) {
+            const textStyle = `${postingContentsStyle.p[key as keyof typeof postingContentsStyle.p]}`;
+            textStyleArr.push(textStyle);
+        } else if (key === "color") {
+            const colorValue = text.rich_text[0].annotations[key];
+
+            if (colorValue !== "default") {
+                const colorStyle = notionColorStyle[colorValue];
+                textStyleArr.push(colorStyle);
+            }
+        }
+    }
+
+    return <p className={`${textStyleArr.join(" ")}`}>{text.rich_text[0].plain_text}</p>;
 };

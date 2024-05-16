836f66a366e32c395a27f2f1ebb54cfee2761ee4
pparksang <pparksang1013@gmail.com>
2024-05-16 18:58:48 +0900

    ⭐️FEAT br 태그 기능 추가

--- components/posting/child/Paragraph.tsx
+++ components/posting/child/Paragraph.tsx
@@ -1,9 +1,5 @@
 import { RichTextType } from "@/type/notionRichtextType";
 
 export const Paragraph = ({ text }: { text: RichTextType }) => {
-    if (!text.rich_text.length) {
-        return;
-    }
-
-    return <p>{text.rich_text[0].plain_text}</p>;
+    return text.rich_text.length !== 0 ? <p>{text.rich_text[0].plain_text}</p> : <br />;
 };

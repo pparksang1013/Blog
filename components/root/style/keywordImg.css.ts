c48bd377d26c3c6448f31f4a1ede07d88af5ba17
pparksang <pparksang1013@gmail.com>
2024-05-16 16:32:13 +0900

    deleted

--- components/root/style/keywordImg.css.ts
+++
@@ -1,9 +0,0 @@
-import { style } from "@vanilla-extract/css";
-
-export const keywordImgStyle = {
-    img: style({
-        position: "absolute",
-        right: 0,
-        bottom: 0,
-    }),
-};

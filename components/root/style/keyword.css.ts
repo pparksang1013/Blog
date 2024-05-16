dbff458a9b27c927c09ca2f6cf91a19e16b55e9a
pparksang <pparksang1013@gmail.com>
2024-05-16 16:39:42 +0900

    ⭐️FEAT Keyword 추가

---
+++ components/root/style/keyword.css.ts
@@ -0,0 +1,11 @@
+import { style } from "@vanilla-extract/css";
+
+export const keywordStyle = {
+    icon: style({
+        fontSize: 22,
+        opacity: 0.7,
+    }),
+    span: style({
+        fontSize: 24,
+    }),
+};

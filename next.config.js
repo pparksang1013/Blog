84a8836ab43c45b6655d43ec0695b593fffff46b
pparksang <pparksang1013@gmail.com>
2024-05-22 02:25:58 +0900

    💊CHORE 노션 이미지 설정 추가
    - 외부에서 불러오는 이미지를 추가하기 위해 remotePatterns 추가

--- next.config.js
+++ next.config.js
@@ -2,6 +2,18 @@ const { createVanillaExtractPlugin } = require("@vanilla-extract/next-plugin");
 const withVanillaExtract = createVanillaExtractPlugin();
 
 /** @type {import('next').NextConfig} */
-const nextConfig = { reactStrictMode: true, swcMinify: false, pageExtensions: ["ts", "tsx", "js", "jsx", "md", "mdx"] };
+const nextConfig = {
+    reactStrictMode: true,
+    swcMinify: false,
+    pageExtensions: ["ts", "tsx", "js", "jsx", "md", "mdx"],
+    images: {
+        remotePatterns: [
+            {
+                protocol: "https",
+                hostname: "*.notion.so",
+            },
+        ],
+    },
+};
 
 module.exports = withVanillaExtract(nextConfig);

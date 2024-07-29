cbba415102d58cd6d9b43a99812b4033aee9d794
pparksang <pparksang1013@gmail.com>
2024-07-29 19:04:06 +0900

    🧩REFACTORING

--- app/components/(blog)/BlogFeed.tsx
+++ app/components/(blog)/BlogFeed.tsx
@@ -7,29 +7,26 @@ import { getDatabase } from "@/app/api/getDatabase";
 import { feedStyle } from "@/app/style/(blog)/blogfeed.css";
 // COMP
 import { Tag } from "../(common)/Tag";
-import { Keyword } from "../(common)/Keyword";
-// ASSET
-import posting_01 from "@/public/img/posting01.jpg";
 
 async function BlogFeed() {
     const notionDatabase = await getDatabase();
-    const imgArr = [posting_01];
 
     const feed = notionDatabase.map((ele, i) => {
         const date = format(new Date(ele.date), "yy. MM");
 
         return (
             <>
-                <Link href={`posting/${ele.id}`} className={feedStyle.link} key={ele.id}>
-                    <div className={feedStyle.imgBox}>
-                        <Image src={imgArr[i]} alt="posting img" objectFit="cover" fill className={feedStyle.img} />
+                <Link href={`posting/${ele.id}`} className={feedStyle.link} key={i}>
+                    <div className={feedStyle.left}>
                         <span className={feedStyle.date}>{date}</span>
-                        <Keyword keyword={ele.keyword} />
                     </div>
 
                     <div className={feedStyle.contentsWrapper}>
-                        <h1 className={feedStyle.title}>{ele.title}</h1>
+                        <h1 className={feedStyle.title}>
+                            {ele.title} <b className={feedStyle.arrow}>🡒</b>
+                        </h1>
                         <Tag tags={ele.tags} />
+                        <span className={feedStyle.emoji}>👀</span>
                     </div>
                 </Link>
             </>
@@ -39,7 +36,7 @@ async function BlogFeed() {
     return (
         <main>
             <h2 className={feedStyle.feedText}>feed.</h2>
-            <div className={feedStyle.wrapper}>{feed}</div>
+            <div>{feed}</div>
         </main>
     );
 }
